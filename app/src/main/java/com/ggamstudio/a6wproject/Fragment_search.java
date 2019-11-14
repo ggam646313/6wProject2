5e9b4863f2d7c9a216b37dca736d002c539f4b49
ggam <wjstmdgusz@naver.com>
2019-11-14 17:28:59 +0900

    11 14

--- app/src/main/java/com/ggamstudio/a6wproject/Fragment_search.java
+++ app/src/main/java/com/ggamstudio/a6wproject/Fragment_search.java
@@ -9,16 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Fragment_home extends Fragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class Fragment_search extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        return inflater.inflate(R.layout.fragment_search,container,false);
     }
 }
--- app/src/main/java/com/ggamstudio/a6wproject/MainActivity.java
+++ app/src/main/java/com/ggamstudio/a6wproject/MainActivity.java
@@ -1,22 +1,80 @@
 package com.ggamstudio.a6wproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private FragmentManager fragmentManager = getSupportFragmentManager();
+//
+//
+//    private Fragment_home fragment_home = new Fragment_home();
+//    private Fragment_search fragment_search = new Fragment_search();
+//    private Fragment_upload fragment_upload = new Fragment_upload();
+//    private Fragment_favorite fragment_favorite = new Fragment_favorite();
+//    private Fragment_my fragment_my = new Fragment_my();
+//
+//    BottomNavigationView bnv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        bnv= findViewById(R.id.bnv);
+//
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.frame_layout, fragment_home).commitAllowingStateLoss();
+//
+//        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                FragmentTransaction transaction = fragmentManager.beginTransaction();
+//
+//                switch (item.getItemId()) {
+//                    case R.id.bnv_home: {
+//                        transaction.replace(R.id.frame_layout, fragment_home).commitAllowingStateLoss();
+//                        break;
+//                    }
+//                    case R.id.bnv_search: {
+//                        transaction.replace(R.id.frame_layout, fragment_search).commitAllowingStateLoss();
+//                        break;
+//                    }
+//                    case R.id.bnv_upload: {
+//                        transaction.replace(R.id.frame_layout, fragment_upload).commitAllowingStateLoss();
+//                        break;
+//                    }
+//                    case R.id.bnv_favorite: {
+//                        transaction.replace(R.id.frame_layout, fragment_favorite).commitAllowingStateLoss();
+//                        break;
+//                    }
+//                    case R.id.bnv_my:{
+//                        transaction.replace(R.id.frame_layout, fragment_my).commitAllowingStateLoss();
+//                        break;
+//                    }
+//                }
+//
+//
+//                return true;
+//            }
+//        });
+
     }
 
     public void clickLogin(View view) {
-        Intent intent= new Intent(this, Fragment_home.class);
+        Intent intent= new Intent(this,Fragment_home.class);
         startActivity(intent);
     }
 }
